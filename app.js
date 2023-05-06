b3cff7365c4d55e1c59d752ff9f8fba0dbcc74a6
humanity1111 <xoliqovmuzaffar240@gmail.com>
2023-05-06 15:14:33 +0500

    JS-start-easy-task

---
+++ app.js
@@ -0,0 +1,31 @@
+let access = confirm ("anketa to'ldirishga tayyormisiz malumotlaringiz console da")
+// if (access === true){
+//     accessRight
+//     else {
+//         alert("tayyor bo'lganingizda ko'rishguncha")
+//     }
+// }
+// function accessRight (){
+    let firstName = prompt ('ismingiz kim');
+    console.log(firstName)
+    
+    let lastName = prompt ('familiyangiz kim');
+    console.log(lastName)
+    
+    let fullName = prompt ('otangiz ismi kim');
+    console.log(fullName)
+    
+    let age = prompt ('yoshingiz nechida');
+    console.log(age)
+    
+    let address = prompt ('yashash joyingiz qayerda');
+    console.log(address)
+    
+    let email = prompt ("emailingizni kiriting");
+    console.log(email)
+    
+    let phone = prompt ('bu yerga faqat telefon raqamingizni kiriting');
+    console.log(Number(phone))
+    
+    alert ("anketa to'ldirganingiz uchun rahmat");
+// }
